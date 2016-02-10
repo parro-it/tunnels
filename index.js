92e0a6e2416858abb3f716d38bf14c279a164a05
Andrea Parodi <andrea@parro.it>
2016-02-10 17:52:22 +0100

    made app single instance

--- index.js
+++ index.js
@@ -3,13 +3,35 @@ const electron = require('electron');
 const electronDebug = require('electron-debug');
 const electronDetach = require('electron-detach');
 const window = require('electron-window');
+const debug = require('debug')('tunnels');
+
 
 if (electronDetach({ requireCmdlineArg: false })) {
+  let listWindow;
+  let closingApp = false;
+
+  const focusMainWindow = () => {
+    debug('focusMainWindow - listWindow is null:', listWindow === null);
+
+    if (listWindow) {
+      listWindow.show();
+    }
+  };
+
+  if (electron.app.makeSingleInstance(focusMainWindow)) {
+    debug('second app instance called. Exit');
+    electron.app.exit(0);
+    return;
+  }
+
   electronDebug();
 
-  electron.app.on('ready', () => {
+  electron.app.on('activate', focusMainWindow);
 
-    const listWindow = window.createWindow({
+  electron.app.on('ready', () => {
+    debug('app ready');
+
+    listWindow = window.createWindow({
       width: 750,
       height: 530,
       titleBarStyle: 'hidden',
@@ -17,9 +39,26 @@ if (electronDetach({ requireCmdlineArg: false })) {
       icon: __dirname + '/src/IconTemplate.png'
     });
 
+    electron.app.on('before-quit', () => {
+      debug('app before-quit');
+      closingApp = true;
+      listWindow.close();
+    });
+
+    listWindow.on('close', e => {
+      if (closingApp) {
+        return;
+      }
+      debug('preventing window close');
+      e.preventDefault();
+      listWindow.hide();
+    });
+
     listWindow.setMenu(null);
     const indexPath = __dirname + '/src/index.html';
     listWindow.showUrl(indexPath);
   });
+
+
 }
 
