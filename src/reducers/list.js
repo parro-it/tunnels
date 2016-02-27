77825c4515969d9d2e7d96323130bff60d616afa
Andrea Parodi <andrea@parro.it>
2016-02-27 21:12:38 +0100

    extracted tunnel reducer

--- src/reducers/list.js
+++ src/reducers/list.js
@@ -1,32 +1,4 @@
-function tunnel(state = {}, action) {
-  const result = action.payload.result;
-  switch (action.type) {
-    case 'OPEN_TUNNEL_STATE':
-      if (result === 'success') {
-        return { ...state, status: 'open' };
-      } else if (result === 'failure') {
-        return { ...state, status: 'close' };
-      }
-      return { ...state, status: result };
-
-    case 'CLOSE_TUNNEL_STATE':
-      if (result === 'success') {
-        return { ...state, status: 'close' };
-      } else if (result === 'failure') {
-        return { ...state, status: 'open' };
-      }
-      return { ...state, status: result };
-
-    case 'SAVE_TUNNEL':
-      return {
-        ...action.tunnel,
-        status: state.status
-      };
-
-    default:
-      return state;
-  }
-}
+import tunnel from './tunnel';
 
 export default function list(state = [], action) {
   switch (action.type) {
