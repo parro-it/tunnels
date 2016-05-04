60e3ebca8198169157ccd324b226d91522cd46c8
Andrea Parodi <andrea@parro.it>
2016-05-04 18:07:33 +0200

    scripts improvements

--- scripts/pkg/build.sh
+++ scripts/pkg/build.sh
@@ -1,7 +1,8 @@
+rm -rf setup/${npm_package_productName}-${platform}-${arch};
 electron-packager dist ${npm_package_productName} \
 	--no-asar \
 	--platform=${platform} \
 	--arch=${arch} \
 	--overwrite \
-	--out=setup
+	--out=setup;
 
--- scripts/pkg/compile.sh
+++ scripts/pkg/compile.sh
@@ -1,7 +1,10 @@
 rm -rf dist;
+mkdir dist
 babel src --out-dir dist/src;
-cp *.js *.json src/*.css src/*.html src/*.png dist;
+cp *.js *.json dist;
+cp src/*.css src/*.html src/*.png dist/src;
 cd dist;
 npm i --production;
+node_modules/.bin/electron-rebuild -e ../node_modules/electron-prebuilt;
 cd ..;
 
